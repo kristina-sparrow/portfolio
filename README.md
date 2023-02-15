61ecbe402cafeea42a91a4ba9fdfa727eab3e1e7
kristina-sparrow <118679841+kristina-sparrow@users.noreply.github.com>
2023-02-15 16:06:59 -0700

    Install tools

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# portfolio
+# My Portfolio Website
+
+A personal portfolio website built with React and SASS to showcase my projects.
+
+[Live Link](https://kristinasparrow.com/) :point_left:
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "src/index.js"),
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+            ],
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  devServer: {
+    static: ["src"],
+    hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+  ],
+};

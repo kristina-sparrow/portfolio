ac1ff827531c2376903076e3248c97a0e2c8d47b
kristina-sparrow <118679841+kristina-sparrow@users.noreply.github.com>
2023-02-16 13:41:27 -0700

    Create intro component and add placeholder images

--- src/app/components/Intro.js
+++ src/app/components/Intro.js
@@ -2,6 +2,12 @@ import React from "react";
 
 export default function Intro() {
   return (
-    <section className="intro">Intro</section>
+    <section className="intro" id="home">
+      <h1 className="section__title section__title--intro">
+        Hi, I am <strong>Kristina Sparrow</strong>
+      </h1>
+      <p className="section__subtitle section__subtitle--intro">front-end dev</p>
+      <img src="../assets/kristina-01.jpg" alt="Kristina Sparrow smiling" className="intro__img" />
+    </section>
   );
 }
